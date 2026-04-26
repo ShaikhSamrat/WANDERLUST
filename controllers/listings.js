b4326782402138ddbae55eb80aef6941f4bd4ae0
Sk Samrat <786samraj@gmail.com>
2026-04-26 17:31:45 +0530

    removed mapbox

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,7 +1,5 @@
 const Listing = require("../models/listing");
-const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-const mapToken = process.env.MAP_TOKEN;
-const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
 
 module.exports.index = async(req,res) => {
     const allListings = await Listing.find({});
@@ -27,12 +25,7 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
-    let response = await geocodingClient
-    .forwardGeocode({
-        query: req.body.listing.location,
-        limit: 1,
-      })
-        .send();
+
 
     let url = req.file.path;
     let filename = req.file.filename;
@@ -40,7 +33,7 @@ module.exports.createListing = async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
     newListing.image = {url, filename};
-    newListing.geometry = response.body.features[0].geometry;
+    
 
     let savedListing = await newListing.save();
     console.log(savedListing);
